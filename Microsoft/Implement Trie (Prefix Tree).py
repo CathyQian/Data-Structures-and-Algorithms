6ce0591b481d4dfd87ccf4d792fe6912ab11c293
zqian <zqian@carrentals.com>
2020-08-03 09:22:47 -0700

    update

---
+++ Microsoft/Implement Trie (Prefix Tree).py
@@ -0,0 +1,77 @@
+
+"""
+Implement Trie (Prefix Tree)
+
+Implement a trie with insert, search, and startsWith methods.
+
+Example:
+
+Trie trie = new Trie();
+
+trie.insert("apple");
+trie.search("apple");   // returns true
+trie.search("app");     // returns false
+trie.startsWith("app"); // returns true
+trie.insert("app");   
+trie.search("app");     // returns true
+
+Note:
+
+    You may assume that all inputs are consist of lowercase letters a-z.
+    All inputs are guaranteed to be non-empty strings.
+
+
+"""
+
+# 1)This problem have multiple solutions, see Google Colab
+# If prefix = word, return True
+class Trie:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = {}
+        self.vals = set() # can easily remove and do a regular search
+
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+        node = self.root
+        for char in word:
+            if char not in node:
+                node[char] = {}
+            node = node[char]
+        node[word] = word 
+        self.vals.add(word)
+        
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        if word in self.vals:
+            return True
+        return False
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        node = self.root
+        for char in prefix:
+            if char not in node:
+                return False
+            else:
+                node = node[char]
+        return True
+        
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
+"""
---
+++ Microsoft/Serialize and Deserialize BST.py
@@ -0,0 +1,60 @@
+"""
+Serialize and Deserialize BST
+
+Serialization is the process of converting a data structure or object into a sequence of bits so that it can be stored in a file or memory buffer, or transmitted across a network connection link to be reconstructed later in the same or another computer environment.
+
+Design an algorithm to serialize and deserialize a binary search tree. There is no restriction on how your serialization/deserialization algorithm should work. You just need to ensure that a binary search tree can be serialized to a string and this string can be deserialized to the original tree structure.
+
+The encoded string should be as compact as possible.
+
+Note: Do not use class member/global/static variables to store states. Your serialize and deserialize algorithms should be stateless.
+
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+
+    def serialize(self, root: TreeNode) -> str:
+        """Encodes a tree to a single string.
+        """
+        res = []
+        q = [root]
+        while q:
+            ele = q.pop(0)
+            if ele:
+                q.append(ele.left)
+                q.append(ele.right)
+            res.append(str(ele.val) if ele else '#')
+        return ','.join(res)
+
+    def deserialize(self, data: str) -> TreeNode:
+        """Decodes your encoded data to tree.
+        """
+        vals = data.split(',')
+        if vals[0] == '#':
+            return None
+        else:
+            root = TreeNode(int(vals[0]))
+        q = [root]
+        i = 1
+        while i < len(vals):
+            ele = q.pop(0)
+            if vals[i] != '#':
+                ele.left = TreeNode(int(vals[i]))
+                q.append(ele.left)
+            i += 1
+            if vals[i] != '#':
+                ele.right = TreeNode(int(vals[i]))
+                q.append(ele.right)
+            i += 1
+        return root
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.deserialize(codec.serialize(root)) 
