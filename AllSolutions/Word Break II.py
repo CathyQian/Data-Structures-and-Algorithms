0376fa46c0343214aa2b025beb180a9812985f9a
CathyQian <zhaoxiaq@gmail.com>
2020-09-06 09:14:08 -0700

    optimize code

--- AllSolutions/Word Break II.py
+++ AllSolutions/Word Break II.py
@@ -1,45 +1,65 @@
 """
-"""
-[140. Word Break II](https://leetcode.com/problems/word-break-ii/)
-"""
-"""
-# not sure why this method always give time exceed limit,
+Word Break II
 
-import collections
+Given a non-empty string s and a dictionary wordDict containing a list of non-empty words, add spaces in s to construct a sentence where each word is a valid dictionary word.
+Return all such possible sentences.
 
-class Solution:
-    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
-        memo = collections.defaultdict(set)
-        wordDict = set(wordDict)
-        self.dfs(s, 0, wordDict, memo)
-        return memo[0]
-    
-    def dfs(self, s, start, wordDict, memo):
-        if start != len(s):
-            if s[start:] in wordDict:
-                memo[start].add(s[start:])
-            for word in wordDict:
-                end = start + len(word)
-                if s[start:end] == word and self.dfs(s, end, wordDict, memo):
-                    memo[start].update(set([s[start:end] + ' ' + p for p in memo[end]]))
-        return True if memo[start] else False
+Note:
+
+    The same word in the dictionary may be reused multiple times in the segmentation.
+    You may assume the dictionary does not contain duplicate words.
+
+Example 1:
+
+Input:
+s = "catsanddog"
+wordDict = ["cat", "cats", "and", "sand", "dog"]
+Output:
+[
+  "cats and dog",
+  "cat sand dog"
+]
+
+Example 2:
+
+Input:
+s = "pineapplepenapple"
+wordDict = ["apple", "pen", "applepen", "pine", "pineapple"]
+Output:
+[
+  "pine apple pen apple",
+  "pineapple pen apple",
+  "pine applepen apple"
+]
+Explanation: Note that you are allowed to reuse a dictionary word.
+
+Example 3:
+
+Input:
+s = "catsandog"
+wordDict = ["cats", "dog", "sand", "and", "cat"]
+Output:
+[]
+
+
 """
+
+# dfs + memo (memo vals are different from word break I; dfs needs res to record path)
 class Solution(object):
     def wordBreak(self, s, wordDict):
-
-        res = []
-        memo = dict()
-        return self.dfs(s, res, wordDict, memo)
+        return self.dfs(s, 0, wordDict, dict())
     
-    def dfs(self, s, res, wordDict, memo):
-        if s in memo: return memo[s]
-        if not s:
+    def dfs(self, s, start, wordDict, memo):
+        """return breaking combination of s[start:] in memo[start] (equivalent to res)
+        """
+        if start == len(s): # test edge case
             return [""]
-        res = []
-        for word in wordDict:
-            if s[:len(word)] != word: continue
-            for r in self.dfs(s[len(word):], res, wordDict, memo):
-                res.append(word + ("" if not r else " " + r))
-        memo[s] = res
-        return res
+        if start not in memo:
+            res = [] # needed to record path along the recursion process, the main difference from Word Break
+            for word in wordDict:
+                if start + len(word) <= len(s) and s[start:start+len(word)] == word:
+                    for r in self.dfs(s, start+len(word), wordDict, memo):
+                        res.append(word + ("" if not r else " " + r))
+            memo[start] = res
+        return memo[start]
 """
