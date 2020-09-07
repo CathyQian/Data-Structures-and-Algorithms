69a17a8f17bd26f30cf39b25b284b53d3876b518
CathyQian <zhaoxiaq@gmail.com>
2020-09-07 09:58:30 -0700

    optimize code

--- AllSolutions/Palindrome Permutation II.py
+++ AllSolutions/Palindrome Permutation II.py
@@ -12,13 +12,14 @@ Hint:
 If a palindromic permutation exists, we just need to generate the first half of the string.
 To generate all distinct permutations of a (half of) string, use a similar approach from: Permutations II or Next Permutation.
 """
+# dfs + trick in permutation (to avoid duplicates, otherwise time exceed limit)
+import collections
 
 class Solution:
     def generatePalindromes(self, s: str) -> List[str]:
         char_count = collections.defaultdict(int)
         for char in s:
-            char_count[char] += 1
-            
+            char_count[char] += 1   
         
         mid_char = ''
         even_chars = []
@@ -31,23 +32,22 @@ class Solution:
                 mid_char = char
                 odd_count += 1
                 even_chars += [char]*(count//2)
-                if odd_count >1:
+                if odd_count > 1:
                     return []
-         
-        half_permu = [] 
-        self.dfs('', half_permu, even_chars)
-        result = []
-        for s in half_permu:
-            result.append(s+mid_char+s[::-1])
-        return result
+        self.result = set()
+        self.permute(even_chars, mid_char, 0)
+        return list(self.result)
     
-    def dfs(self, base, half_permu, even_chars):
-        if not even_chars:
-            half_permu.append(base)
-        curr=''
-        for idx in range(len(even_chars)):
-            if even_chars[idx] == curr:
-                continue
+    def permute(self, s, m, start):
+        if start == len(s):
+            if m:
+                self.result.add(''.join(s) + m + ''.join(s[::-1]))
             else:
-                curr= even_chars[idx]
-                self.dfs(base+curr, half_permu, even_chars[:idx]+even_chars[idx+1:])s
+                self.result.add(''.join(s) + ''.join(s[::-1]))
+        else:
+            for i in range(start, len(s)):
+                if s[start] != s[i] or start == i:
+                    s[start], s[i] = s[i], s[start] 
+                    self.permute(s, m, start+1)
+                    s[start], s[i] = s[i], s[start]
+                    
