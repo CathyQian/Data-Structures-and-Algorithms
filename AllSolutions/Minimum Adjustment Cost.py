f639076988c63ac850be1f14c3169eff1c67f5eb
CathyQian <zhaoxiaq@gmail.com>
2020-09-08 15:29:14 -0700

    Update Minimum Adjustment Cost.py

--- AllSolutions/Minimum Adjustment Cost.py
+++ AllSolutions/Minimum Adjustment Cost.py
@@ -26,11 +26,6 @@ Space complexity: O(n)
 
 
 这是一个背包问题，不太容易看出来。因为数的范围是1~100，每个数有100中调整的可能性，采用动态规划的思路。
-建立大小为(n+1)*101的数组dp记录所有可能调整的代价，初始化为全0.
-dp中第i个iteration对应加入A[i-1]。对于每个数A[i]，调整后的结果有100种，用dp[j]表示数字A[i]调整为j的最小代价。
-对于每个dp[j]，A[i-1]调整到k的代价加上A[i]调整到j的最小代价即为dp[j]的代价。而k又有100种选择，对于j，
-当|j-k|的绝对值不大于target时，代价最小，rec[j]保留所有可能代价中的最小代价。iteration完了返回min(rec[j]).
-start from 2D dp
 dp[i][v]: the value of the ith element in A is modified to v, the min cost
 i range from 0 to len(A) -1, V range from 0 to 100
 2D dp can be replaced by 1D dp using pre and post array
