e0df806ecf621925a0cfc39d5839fa83b86c3f7d
CathyQian <zhaoxiaq@gmail.com>
2020-09-07 17:05:48 -0700

    optimize code

--- AllSolutions/Combination Sum II.py
+++ AllSolutions/Combination Sum II.py
@@ -32,22 +32,22 @@ class Solution:
     def combinationSum2(self, candidates, target):
         candidates.sort()                      
         result = []
-        self.combine_sum_2(candidates, 0, [], result, target)
+        self.dfs(candidates, 0, [], result, target)
         return result
 
-    def combine_sum_2(self, nums, start, path, result, target):
+    def dfs(self, nums, start, path, result, target):
 
         if target == 0:
             result.append(path)
             return
 
         for i in range(start, len(nums)):
-        
+            # there may be duplicates in this array
             if i > start and nums[i] == nums[i - 1]:
                 continue
 
             if nums[i] > target:
                 break
-
-            self.combine_sum_2(nums, i + 1, path + [nums[i]], result, target - nums[i])
+            # each elements can be used only once
+            self.dfs(nums, i + 1, path + [nums[i]], result, target - nums[i])
         return
