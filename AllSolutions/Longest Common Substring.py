c72958079a2e212e6b94693330f20493380eb667
CathyQian <zhaoxiaq@gmail.com>
2020-09-08 07:57:13 -0700

    solution added

--- AllSolutions/Longest Common Substring.py
+++ AllSolutions/Longest Common Substring.py
@@ -21,3 +21,19 @@ Example 2:
 	Longest common substring is 'A' or 'C' or 'B'
 Challenge
 O(n x m) time and memory.
+
+class Solution:
+    def longestSubstring(self, s1, s2):
+	if not s1 or not s2:
+	    return 0
+	m, n = len(s1), len(s2)
+	ans = 0
+	dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
+	for i in range(m):
+	    for j in range(n):
+		if s1[i] == s2[j]:
+		    dp[i+1][j+1] = dp[i][j] + 1
+		    ans = max(ans, dp[i+1][j+1])
+		else: # major difference from subsequence
+		    dp[i+1][j+1] = 0
+        return ans
