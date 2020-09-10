c6a39f41da69b406549e4e85f4ef15a51910287f
CathyQian <zhaoxiaq@gmail.com>
2020-09-10 08:23:33 -0700

    Update Best Time to Buy and Sell Stock IV.py

--- AllSolutions/Best Time to Buy and Sell Stock IV.py
+++ AllSolutions/Best Time to Buy and Sell Stock IV.py
@@ -21,7 +21,7 @@ Explanation: Buy on day 2 (price = 2) and sell on day 3 (price = 6), profit = 6-
 
 
 """
-# state machine
+# state machine (hard to understand)
 class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
         if not prices:
@@ -42,12 +42,25 @@ class Solution:
                 sell[j] = max(pre_sell[j], pre_buy[j]+prices[i])
         return sell[-1]
 
-# method 2, dp
+# method 2, dp (preferred)
+"""
+ref: https://www.cnblogs.com/grandyang/p/4295761.html
+我们定义local[i][j]为在到达第i天时最多可进行j次交易并且最后一次交易在最后一天卖出的最大利润，此为局部最优。然后我们定义global[i][j]为在到达第i天时最多可进行j次交易的最大利润，此为全局最优。
+它们的递推式为：
+local[i][j] = max(global[i - 1][j - 1] + max(diff, 0), local[i - 1][j] + diff) # local[i-1][j] not local[i-1][j-1], sale at (i-1)th day vs sale at ith day, while number of 
+transactions doesn't change.
+global[i][j] = max(local[i][j], global[i - 1][j])，
+其中局部最优值是比较前一天并少交易一次的全局最优加上大于0的差值，和前一天的局部最优加上差值后相比，两者之中取较大值，而全局最优比较局部最优和前一天的全局最优。
+
+change 2D array into 1D array, if j grow incrementally, l and g with smaller j will be updated first, so we still need 2D array. Therefore, j needs to decrease gradually, so 
+we can still use 1D array to save some space.
+"""
 class Solution:
     def maxProfit(self, k, prices):
         if not prices:
             return 0
-        
+          
+        # Can buy and sell anytime, no constraints
         if k >= len(prices)//2:
             profit = 0
             for i in range(1, len(prices)):
@@ -56,7 +69,7 @@ class Solution:
                     profit += diff
             return profit
         
-        l, g = [0] * (k+1), [0] * (k+1)
+        l, g = [0] * (k+1), [0] * (k+1) # local and global max profit at the ith transactions (i from 1 to k)
         for i in range(1, len(prices)):
             diff = prices[i] - prices[i - 1]
             for j in range(1, k+1)[::-1]: # 1D dp to save space, scan j reversed
