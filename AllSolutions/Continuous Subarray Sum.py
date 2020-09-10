dc96b0d4c933dcb01c4a1ab50ba44f22db328689
CathyQian <zhaoxiaq@gmail.com>
2020-09-10 08:39:41 -0700

    Update Continuous Subarray Sum.py

--- AllSolutions/Continuous Subarray Sum.py
+++ AllSolutions/Continuous Subarray Sum.py
@@ -28,36 +28,25 @@ Note:
 """
 # method 1 Hash Table, to restore modulus or reminder, O(n)
 from collections import defaultdict
-class Solution:
-    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        n = len(nums)
-        sums = nums[:]
-        for i in range(1,n):
-            sums[i] += sums[i-1]
-        
-        mods = defaultdict(list)
-        mods[0] = [-1] # corner case, when there is no element we have modulus as zero/when k = 0, the first 0 in sums appear, return True
-        for idx, s in enumerate(sums):
-            if k: # k != 0
-                mod = s%k
-                if mod in mods and idx - mods[mod][0]> 1 :
-                    return True
-                else:
-                    mods[mod].append(idx)
-            else: # k == 0
-                if s in mods and idx-mods[s][0]>1:
-                    return True
-                else:
-                    mods[s].append(idx)
-        return False
 
-# method 2: brutal force, O(n2)
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        for i in range(len(nums)-1):
-            sums = nums[i]
-            for j in range(i+1, len(nums)):
-                sums += nums[j]
-                if (k != 0 and sums%k == 0) or (sums == 0 and k == 0):
-                    return True            
+        if not nums:
+            return False
+        sums = 0
+        mods = collections.defaultdict(list)
+        mods[0] = [-1]
+        for i in range(len(nums)):
+            sums += nums[i]
+            if k: # k != 0
+                mod = sums%k
+                if mod in mods and i - mods[mod][0]> 1 :
+                    return True
+                else:
+                    mods[mod].append(i)
+            else: # k == 0
+                if sums in mods and i-mods[sums][0]>1:
+                    return True
+                else:
+                    mods[sums].append(i)
         return False
