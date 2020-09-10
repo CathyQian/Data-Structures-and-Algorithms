b65ae82b3c99e98045a610d4dadae26d7f9b6766
CathyQian <zhaoxiaq@gmail.com>
2020-09-10 06:04:14 -0700

    Update Different Ways to Add Parentheses.py

--- AllSolutions/Different Ways to Add Parentheses.py
+++ AllSolutions/Different Ways to Add Parentheses.py
@@ -28,10 +28,6 @@ Explanation:
 
 class Solution(object):
     def diffWaysToCompute(self, input):
-        """
-        :type input: str
-        :rtype: List[int]
-        """
         if input.isdigit():
             return [eval(input)]
         res = []
