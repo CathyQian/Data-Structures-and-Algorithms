dac3830fad2d0b7bed009fcf5cfea4f2fca59f75
CathyQian <zhaoxiaq@gmail.com>
2020-09-10 17:44:14 -0700

    optimize code

--- AllSolutions/Minimum Increment to Make Array Unique.py
+++ AllSolutions/Minimum Increment to Make Array Unique.py
@@ -27,3 +27,44 @@ Note:
     0 <= A.length <= 40000
     0 <= A[i] < 40000
 """
+
+# brutal force, time exceed limit
+class Solution:
+    def minIncrementForUnique(self, A: List[int]) -> int:
+        A_set = set()
+        moves = 0
+        for a in A:
+            while a in A_set:
+                a += 1
+                moves += 1
+            A_set.add(a)
+        return moves
+       
+ # sort A first     
+ class Solution:
+     def minIncrementForUnique(self, A: List[int]) -> int:
+         A.sort()
+         need, res = 0, 0
+         for a in A:
+             res += max(need-a, 0)
+             need = max(need, a) + 1
+         return res
+        
+# what if there are lots of duplicates? use hashmap and ordered dict
+import collections
+class Solution:
+    def minIncrementForUnique(self, A: List[int]) -> int:
+        A.sort()
+        res, need = 0, 0
+        numCnt = collections.OrderedDict()
+        for a in A:
+            if a not in numCnt:
+                numCnt[a] = 1
+            else:
+                numCnt[a] += 1
+        while numCnt:
+            num, count = numCnt.popitem(last=False)
+            res += max((need - num)*count, 0) + count*(count-1)//2
+            need = max(num, need) + count
+            
+        return res
