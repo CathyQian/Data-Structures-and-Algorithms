382001eca9ce8c37d657b27b9168749d1e149b7c
CathyQian <zhaoxiaq@gmail.com>
2020-09-15 08:41:44 -0700

    Update Same Tree.py

--- AllSolutions/Same Tree.py
+++ AllSolutions/Same Tree.py
@@ -44,7 +44,4 @@ class Solution:
     def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
         if p is None and q is None:
             return True
-        elif p and q and p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right):
-            return True
-        else:
-            return False
+        return p and q and p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
