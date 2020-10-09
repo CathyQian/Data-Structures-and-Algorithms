52580e59f8565c41194907e9609f33ba8e28caee
CathyQian <zhaoxiaq@gmail.com>
2020-10-08 17:15:35 -0700

    Update Walls and Gates.py

--- AllSolutions/Walls and Gates.py
+++ AllSolutions/Walls and Gates.py
@@ -34,37 +34,6 @@ results of some cells wrong.
 The right way is to start bfs or dfs from gates
 """
 
-# wrong solution
-class Solution:
-    def wallsAndGates(self, rooms: List[List[int]]) -> None:
-        """
-        Do not return anything, modify rooms in-place instead.
-        """
-        if not rooms or not rooms[0]:
-            return
-        self.m, self.n = len(rooms), len(rooms[0])
-        for i in range(self.m):
-            for j in range(self.n):
-                if rooms[i][j] == 2**31 - 1:
-                    self.dfs(rooms, i, j)
-        return 
-    
-    def dfs(self, rooms, i, j):
-        rooms[i][j] = -1
-        neighbors = [2**31-1]
-        for x, y in [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]:
-            if 0 <= x < self.m and 0 <= y < self.n and rooms[x][y] != -1:
-                if rooms[x][y] == 2**31 - 1:
-                    self.dfs(rooms, x, y)
-                if rooms[x][y] != 2**31 - 1:
-                    neighbors.append(rooms[x][y]+1)
-
-        rooms[i][j] = min(neighbors)
-
-# Test case to prove it wrong, only rooms[0][13] gets the wrong distance
-# [[0,2147483647,2147483647,0,-1,-1,0,0,0,-1,-1,0,2147483647,2147483647],[2147483647,-1,2147483647,-1,2147483647,0,-1,2147483647,-1,2147483647,2147483647,-1,-1,2147483647],[0,0,-1,2147483647,-1,2147483647,-1,-1,2147483647,0,0,2147483647,0,2147483647],[-1,0,2147483647,-1,0,0,-1,2147483647,0,2147483647,0,-1,0,-1]]
-
-
 # right solution -- dfs
 class Solution:
     def wallsAndGates(self, rooms: List[List[int]]) -> None:
@@ -83,10 +52,8 @@ class Solution:
         if i < 0 or i >= self.m or j < 0 or j >= self.n or rooms[i][j] < val:
             return
         rooms[i][j] = val
-        self.dfs(rooms, i + 1, j, val + 1)
-        self.dfs(rooms, i - 1, j, val + 1)
-        self.dfs(rooms, i, j + 1, val + 1)
-        self.dfs(rooms, i, j - 1, val + 1)
+        for x, y in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+            self.dfs(rooms, i + x, j + y, val + 1)
         
 # right solution, bfs (i like this better)
 class Solution:
