4397ecaea355ea3868bc283b128e643b305476dc
CathyQian <zhaoxiaq@gmail.com>
2020-10-13 10:59:06 -0700

    Update Shortest Substring.py

--- AllSolutions/Shortest Substring.py
+++ AllSolutions/Shortest Substring.py
@@ -3,24 +3,4 @@
 所有字典中的字母。 应该是leetcode的原题了了。 做法是two pointer + hashmap
 """
 
-from collections import defaultdict 
-def find_min_window(S, T):     
-    left, right = 0, 0     
-    c = defaultdict(int)    
-    for ch in T: # scan the dictionary, hashmap allows for O(1) search     
-        c[ch] += 1     
-    min_str = ''         
-    while right <= len(S):  # search the string        
-        if all(map(lambda x: True if x <= 0 else False, c.values())): # all elements in dic have been visited           
-            if not min_str or right-left <= len(min_str): # if the first str or shorter than existing str
-                min_str = S[left:right]             
-            if S[left] in c:           # if found string is longer than existing str, left += 1, then keep searching      
-                c[S[left]] += 1             
-            left += 1         
-        else:             
-            if right == len(S): 
-                break             
-            if S[right] in c:                 
-                c[S[right]] -= 1             
-            right += 1     
-    return min_str
+The same as Minimum Window Substring.
