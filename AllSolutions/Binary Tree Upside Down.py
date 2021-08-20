3622325d74d422796097e8a661c7740f472a3b96
CathyQian <zhaoxiaq@gmail.com>
2021-08-20 12:01:23 -0700

    Update Binary Tree Upside Down.py

--- AllSolutions/Binary Tree Upside Down.py
+++ AllSolutions/Binary Tree Upside Down.py
@@ -58,3 +58,26 @@ class Solution(object):
         root.left = None
         root.right = None
         return left
+    
+    
+# solution2  --- this is the correct solution. The problem is poorly designed.
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def upsideDownBinaryTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root or not root.left:
+            return root
+        left = root.left
+        
+        leftroot = self.upsideDownBinaryTree(root.left)
+        rightroot = self.upsideDownBinaryTree(root.right)
+        root.left, root.right = None, None
+        left.right = root
+        left.left = rightroot
+        
+        return leftroot
+    
