55dc09788fb331ace05d9968a5b152f913830992
CathyQian <zhaoxiaq@gmail.com>
2021-09-14 15:05:16 -0700

    add iterative solution

--- AllSolutions/Validate Binary Search Tree.py
+++ AllSolutions/Validate Binary Search Tree.py
@@ -36,6 +36,7 @@ Explanation: The root node's value is 5 but its right child's value is 4.
 #         self.left = None
 #         self.right = None
 
+# recursive solution
 class Solution:
     def isValidBST(self, root: TreeNode) -> bool:
         return self.isValidBSTRecu(root, float("-inf"), float("inf"))
@@ -47,3 +48,33 @@ class Solution:
         return low < root.val and root.val < high \
                and self.isValidBSTRecu(root.left, low, root.val) \
                and self.isValidBSTRecu(root.right, root.val, high)
+
+# another way to write the code
+class Solution:
+   def isValidBST(self, root, left = float('-inf'), right = float('inf')):
+        return not root or left < root.val < right and \
+                self.isValidBST(root.left, left, root.val) and \
+                self.isValidBST(root.right, root.val, right)
+   
+# iterative solution
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+
+        if not root:
+            return True
+        
+        stack=[]
+        cur = root
+        pre = -sys.maxsize
+        
+        while cur or stack:
+            while cur:
+                stack.append(cur)
+                cur=cur.left
+            cur = stack.pop()
+            if cur.val <= pre:
+                return False
+            pre = cur.val
+            cur = cur.right
+            
+        return True
