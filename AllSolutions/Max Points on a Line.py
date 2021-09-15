0606a923192853e1f8eaee97741d43e604d98bf6
CathyQian <zhaoxiaq@gmail.com>
2021-09-15 14:38:33 -0700

    Update Max Points on a Line.py

--- AllSolutions/Max Points on a Line.py
+++ AllSolutions/Max Points on a Line.py
@@ -25,23 +25,23 @@ Constraints:
 
 """
 class Solution:
-    def maxPoints(self, points):
-        l = len(points)
+    def maxPoints(self, points: List[List[int]]) -> int:
         m = 0
-        for i in range(l):
+        for i in range(len(points)):
             dic = {'i': 1}
+            x, y = points[i][0], points[i][1]
             same = 0
-            for j in range(i+1, l):
+            for j in range(i+1, len(points)):
                 tx, ty = points[j][0], points[j][1]
-                if tx == points[i][0] and ty == points[i][1]: # same points
+                if tx == x and ty == y:
                     same += 1
-                    continue
-                if points[i][0] == tx: # same x
+                elif tx == x:
                     slope = 'i'
-                else: # different x, differnt y
-                    slope = (points[i][1]-ty) * 1.0 /(points[i][0]-tx)
-                if slope not in dic: 
-                    dic[slope] = 1
-                dic[slope] += 1
+                else:
+                    slope = (ty-y)*1.0/(tx-x)
+                if slope not in dic:
+                    dic[slope] = 2
+                else:
+                    dic[slope] += 1
             m = max(m, max(dic.values()) + same)
         return m
